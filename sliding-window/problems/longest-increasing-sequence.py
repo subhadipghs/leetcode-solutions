48d555e5fabd7645c2ffdf24a20a1cd059dd7a17
Subhadip Ghosh <ghoshsubhadipghosh@gmail.com>
2022-02-11 23:50:57 +0530

    Added: sliding window solution

--- sliding-window/problems/longest-increasing-sequence.py
+++ sliding-window/problems/longest-increasing-sequence.py
@@ -4,7 +4,7 @@ import unittest
 
 
 class Solution:
-    # Naive approach
+    # Naive approach with O(n^2) complexity
     def lengthOfTheLongestSubstringNaive(self, s: str) -> int:
         maxLength = 0  # initial value
         for i in range(len(s)):
@@ -24,6 +24,26 @@ class Solution:
                 maxLength = count
         return maxLength
 
+    # Sliding window principle
+    def lengthOfTheLongestSubstring(self, s: str) -> int:
+        left = 0
+        right = 0
+        charMap = {}
+        length = 0
+        while True:
+            if left >= len(s) or right >= len(s):
+                break
+            if charMap.get(s[right]) == None:
+                charMap[s[right]] = s[right]
+                right += 1
+            else:
+                charMap.pop(s[left])
+                left += 1
+                range = abs(left - right) + 1
+                if range > length:
+                    length = range
+        return length
+
 
 class TestCases(TestCase):
     def __init__(self, methodName: str = ...) -> None:
@@ -41,6 +61,10 @@ class TestCases(TestCase):
     def test_3(self):
         self.assertEqual(
             self.solution.lengthOfTheLongestSubstring("pwwkew"), 3)
+
+    def test_4(self):
+        self.assertEqual(
+            self.solution.lengthOfTheLongestSubstring(" "), 1)
 
 
 if __name__ == "__main__":
